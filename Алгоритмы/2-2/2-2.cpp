385e65eb0e66e5022069f3cbf9c3cb1dc957662b
AlexSem51 <117209796+AlexSem51@users.noreply.github.com>
2022-11-02 23:19:50 +0500

    Update 2-2.cpp

--- Алгоритмы/2-2/2-2.cpp
+++ Алгоритмы/2-2/2-2.cpp
@@ -1,1 +1,30 @@
- #include <iostream>  using namespace std; intmain () {//**Условие**: посчитать произведение N натуральных чисел.int n; int product = 1;cin >> n;if (n <= 0){    cout << "Невозможно посчитать произведение натуральных чисел при n <= 0" << endl;}else{    for (int i = 1; i <= n; i++)    {        product *= i;    }    cout << product << endl;}}
+
+ 
+#include <iostream>
+  
+using namespace std;
+
+ 
+int
+main () 
+{
+//**Условие**: посчитать произведение N натуральных чисел.
+
+
+int n; int product = 1;
+
+cin >> n;
+
+if (n <= 0)
+{
+    cout << "Невозможно посчитать произведение натуральных чисел при n <= 0" << endl;
+}
+else
+{
+    for (int i = 1; i <= n; i++)
+    {
+        product *= i;
+    }
+    cout << product << endl;
+}
+}
