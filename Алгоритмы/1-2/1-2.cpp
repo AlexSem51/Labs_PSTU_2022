8cf43c1f7a64d2a6feca11aecb2083bf0bed6b2a
AlexSem51 <117209796+AlexSem51@users.noreply.github.com>
2022-11-02 13:53:29 +0500

    Add files via upload

---
+++ Алгоритмы/1-2/1-2.cpp
@@ -0,0 +1,1 @@
+ #include <iostream>  using namespace std; intmain () {float a, b;cin >> a >> b;if (b == 0){    cout << "На ноль делить нельзя!" << endl;}else{    cout << a / b << endl;}}
