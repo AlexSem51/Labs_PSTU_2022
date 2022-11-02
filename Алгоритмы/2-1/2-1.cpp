c688facaf245fbcbf4122e9912b5c2dda39a16e7
AlexSem51 <117209796+AlexSem51@users.noreply.github.com>
2022-11-02 14:10:55 +0500

    Add files via upload

---
+++ Алгоритмы/2-1/2-1.cpp
@@ -0,0 +1,1 @@
+ #include <iostream>  using namespace std; intmain () {// **Условие**: посчитать сумму N натуральных чисел.int n; int sum = 0;cin >> n;if (n <= 0){    cout << "Невозможно посчитать сумму натуральных чисел при n <= 0" << endl;}else{    for (int i = 1; i <= n; i++)    {        sum += i;    }    cout << sum << endl;}}
