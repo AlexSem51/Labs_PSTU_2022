a313d943081ac8cdf79ac34ef266f7af3f9a7cc2
AlexSem51 <117209796+AlexSem51@users.noreply.github.com>
2022-11-23 11:28:54 +0500

    Add files via upload

---
+++ Алгоритмы/2-8/2-8.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+   
+    
+int n;
+
+cin >> n;
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j <= i; j++)
+    {
+        cout << "*";
+    }
+    cout << endl;
+}
+
+}
