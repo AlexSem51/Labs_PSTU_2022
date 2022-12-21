3c104ce286baa3435f786d3bd8214bf57a814f76
AlexSem51 <117209796+AlexSem51@users.noreply.github.com>
2022-12-21 12:53:01 +0500

    Create 1. Half_method.cpp

---
+++ Алгоритмы/Equations/1. Half_method.cpp
@@ -0,0 +1,31 @@
+
+// f(x) = (x - (1/ 3+ sin(3,6*x)));
+// [0, 0.85];
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    float a, b, c;
+    float fA, fB, fC;
+    double eps = 0.000001;
+    a = 0;
+    b = 0.85;
+    while (b - a > eps)
+    {
+        c = (a + b) / 2;
+        fA = (a - (1/ 3+ sin(3,6*a)));;
+        fB = (b - (1/ 3+ sin(3,6*b));;
+        fC = (c - (1/ 3+ sin(3,6*c)));;
+        if (fA * fC < 0) { b = c; }
+        else if (fC * fB < 0) { a = c; }
+        else { cout << "×òî-òî íå òî!" << endl; }
+    }
+
+    cout << "Êîðíè: " << a << "; " << b << endl;
+
+    return 0;
+}
