05477246f279cdd150a59d4d48c64b1a4861fb9b
AlexSem51 <117209796+AlexSem51@users.noreply.github.com>
2023-05-12 12:30:22 +0500

    Create fraction.h

---
+++ Алгоритмы/Semestr ||/Classes/lab4/fraction.h
@@ -0,0 +1,25 @@
+
+#ifndef MAIN_CPP_FRACTION_H
+#define MAIN_CPP_FRACTION_H
+#include "pair.h"
+
+class fraction: public pair{
+    //first is numerator, second is denominator
+public:
+    fraction();
+    fraction(int,int);
+    fraction(const fraction&);
+    ~fraction();
+    int get_numerator() const{return getFirst();}
+    int get_denominator() const{return getSecond();}
+    void set_numerator(int num){ setFirst(num);}
+    void set_denominator(int den){ setSecond(den);}
+    void operator=(const fraction& frac);
+    friend std::istream& operator>>(std::istream &in, fraction& frac);
+    friend std::ostream& operator<<(std::ostream &out, fraction& frac);
+    bool operator==(fraction frac){ return get_numerator()*frac.get_denominator()==get_denominator()*frac.get_numerator();}
+    bool operator>(fraction frac){ return get_numerator()*frac.get_denominator()>get_denominator()*frac.get_numerator();}
+    bool operator<(fraction frac){ return !((*this==frac) || (*this>frac));}
+};
+
+#endif //MAIN_CPP_FRACTION_H
