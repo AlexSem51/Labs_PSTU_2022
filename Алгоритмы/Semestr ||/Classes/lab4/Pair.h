d9c67f5b6ab5cc2406dea50a7e2d6c2bbf8edf86
AlexSem51 <117209796+AlexSem51@users.noreply.github.com>
2023-05-12 12:32:06 +0500

    Create pair.h

---
+++ Алгоритмы/Semestr ||/Classes/lab4/Pair.h
@@ -0,0 +1,29 @@
+//
+// Created by 1 on 31.03.2023.
+//
+
+#ifndef MAIN_CPP_PAIR_H
+#define MAIN_CPP_PAIR_H
+#include "iostream"
+
+class pair{
+    int first;
+    int second;
+public:
+    pair();
+    pair(int f,int s);
+    pair(const pair& p);
+    ~pair();
+    int getFirst() const{return first;}
+    int getSecond() const{return second;}
+    void setFirst(int f){first=f;}
+    void setSecond(int s){second=s;}
+    void operator=(const pair& p);
+    friend std::istream& operator>>(std::istream &in, pair& p);
+    friend std::ostream& operator<<(std::ostream &out, pair& p);
+    bool operator==(pair p){ return first==p.first && second==p.second;}
+    bool operator>(pair p){ return (first>p.first || (first==p.first && second>p.second));}
+    bool operator<(pair p){ return !((*this==p) || (*this>p));}
+};
+
+#endif //MAIN_CPP_PAIR_H
