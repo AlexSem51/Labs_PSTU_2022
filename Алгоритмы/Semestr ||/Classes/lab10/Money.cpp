d66be30c7fdeebb35c56f43fc260d5b63f80a0b9
AlexSem51 <117209796+AlexSem51@users.noreply.github.com>
2023-05-22 12:34:59 +0500

    Create Money.cpp

---
+++ Алгоритмы/Semestr ||/Classes/lab10/Money.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include "Money.h"
+using namespace std;
+
+Money::Money()
+{
+	rub = 0;
+	kop = 0;
+}
+Money::Money(long RUB, int KOP)
+{
+	rub = RUB;
+	kop = KOP;
+}
+Money::Money(Money& tmp)
+{
+	rub = tmp.rub;
+	kop = tmp.kop;
+}
+Money::~Money() {};
+long Money::get_rub()
+{
+	return rub;
+}
+int Money::get_kop()
+{
+	return kop;
+}
+void Money::set_rub(long RUB)
+{
+	rub = RUB;
+}
+void Money::set_kop(int KOP)
+{
+	kop = KOP;
+}
+istream& operator>>(istream& in, Money& A)
+{
+	cout << "rub?: "; in >> A.rub;
+	cout << "kop?: "; in >> A.kop;
+	return in;
+}
+ostream& operator<<(ostream& out, Money& A)
+{
+	return (out << A.rub << "," << A.kop);
+}
+Money& Money::operator=(const Money& A)
+{
+	if (&A == this)
+	{
+		return *this;
+	}
+	rub = A.rub;
+	kop = A.kop;
+	return *this;
+}
+Money Money::operator+(Money& A)
+{
+	int tmp1 = rub * 100 + kop;
+	int tmp2 = A.rub * 100 + A.kop;
+	Money tmp;
+	tmp.rub = (tmp1 + tmp2) / 100;
+	tmp.kop = (tmp1 + tmp2) % 100;
+	return tmp;
+}
+Money Money::operator-(Money& A)
+{
+	int tmp1 = rub * 100 + kop;
+	int tmp2 = A.rub * 100 + A.kop;
+	Money tmp;
+	tmp.rub = (tmp1 - tmp2) / 100;
+	tmp.kop = (tmp1 - tmp2) % 100;
+	return tmp;
+}
+bool Money::operator!=(const Money& A)
+{
+	if (rub != A.rub && kop != A.kop)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+fstream& operator>>(fstream& fin, Money& A)
+{
+	fin >> A.rub;
+	fin >> A.kop;
+	return fin;
+}
+fstream& operator<<(fstream& fout, Money& A)
+{
+	fout << A.rub << endl << A.kop << endl;
+	return fout;
+}
