893c3313da48bb732715d351f69b5763eaddf96c
AlexSem51 <117209796+AlexSem51@users.noreply.github.com>
2023-05-22 12:06:43 +0500

    Update Dialog.h

--- Алгоритмы/Semestr ||/Classes/lab8/Dialog.h
+++ Алгоритмы/Semestr ||/Classes/lab8/Dialog.h
@@ -1,1 +1,20 @@
+#pragma once
+#include <iostream>
+#include "List.h"
+#include "Event.h"
+using namespace std;
 
+class Dialog : public List
+{
+protected:
+	int EndState;
+public:
+	Dialog(void);
+	virtual ~Dialog(void);
+	virtual void GetEvent(TEvent&);
+	virtual int Execute();
+	virtual void HandleEvent(TEvent&);
+	virtual void ClearEvent(TEvent&);
+	int Valid();
+	void EndExec();
+};
